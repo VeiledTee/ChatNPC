fe91a3288cd7ab680f5b2fbfbd2d5673c7e1b28e
veiledtee <ethanheavey@outlook.com>
2023-05-30 01:47:59 -0300

    testing and refactor

--- pinecone_delete_and_reset.py
+++ pinecone_delete_and_reset.py
@@ -1,26 +1,37 @@
 import json
 
 import pinecone
+from typing import List
+import time
+from chat import extract_name, get_information, load_file_information, upload, embed
 
-from chat import extract_name, get_information, load_file_information, upload
+
+def delete_all_vectors(names_of_characters) -> None:
+    for i in range(len(names_of_characters)):
+        character: str = list(names.keys())[i]
+        data: str = f"Text Summaries/Summaries/{names_of_characters[character].lower()}.txt"
+        namespace: str = extract_name(data).lower()
+        index.delete(deleteAll=True, namespace=namespace)
+
 
 if __name__ == "__main__":
     WINDOW: int = 3  # how many sentences are combined
     STRIDE: int = 2  # used to create overlap, num sentences we 'stride' over
 
     INDEX_NAME: str = "thesis-index"  # pinecone index name
-    NAMESPACE = "melinda-deek"
+    NAMESPACE = "peter-satoru"
 
-    with open("keys.txt", "r") as key_file:  # initialize
+    with open("keys.txt", "r") as key_file:
+        api_keys = [key.strip() for key in key_file.readlines()]
         pinecone.init(
-            api_key=key_file.readlines()[1],
-            environment=key_file.readlines()[2],
+            api_key=api_keys[1],
+            environment=api_keys[2],
         )
 
-    # index = pinecone.Index('thesis-index')
-    #
+    index = pinecone.Index('thesis-index')
+
     # # upload data and generate query embedding.
-    # embedded_query = embed("what flowers can be found near ashbourne")
+    # embedded_query = embed("what fish can be found near ashbourne")
     #
     # # query Pinecone index and get context for model prompting.
     # responses = index.query(
@@ -42,12 +53,16 @@ if __name__ == "__main__":
     # time.sleep(100)
     # index.delete(delete_all=True, namespace="melinda-deek")
 
-    pinecone.delete_index(INDEX_NAME)  # delete old index
-    print(f"{INDEX_NAME} deleted")
+    # pinecone.delete_index(INDEX_NAME)  # delete old index
+    # print(f"{INDEX_NAME} deleted")
 
     # Open file of characters and load its contents into a dictionary
     with open("Text Summaries/characters.json", "r") as f:
         names = json.load(f)
+
+    delete_all_vectors(names)
+    print('deleted')
+    # time.sleep(100)
 
     # loop through characters and store background in database
     for i in range(len(names)):
@@ -56,15 +71,20 @@ if __name__ == "__main__":
         PROFESSION, SOCIAL_CLASS = get_information(CHARACTER)
         print(CHARACTER, PROFESSION)
 
-        DATA_FILE: str = f"Text Summaries/{names[CHARACTER].lower()}.txt"
+        DATA_FILE: str = f"Text Summaries/Summaries/{names[CHARACTER].lower()}.txt"
 
         NAMESPACE: str = extract_name(DATA_FILE).lower()
-
+        char_info: List[str] = load_file_information(DATA_FILE)
+        # for c in char_info:
+        #     upload(
+        #         NAMESPACE,
+        #         [c],
+        #         "background",
+        #     )
         upload(
             NAMESPACE,
             load_file_information(DATA_FILE),
             "background",
-            INDEX_NAME,
         )
 
         print(f"{NAMESPACE} background uploaded")
