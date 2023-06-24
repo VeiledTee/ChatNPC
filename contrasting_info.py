abfe98e7086c153931790ef2912864a742c1a7e9
VeiledTee <ethanheavey@outlook.com>
2023-06-24 00:28:12 -0300

    Refactoring and visualization improvements

--- contrasting_info.py
+++ contrasting_info.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from chat import embed
 from sklearn.manifold import TSNE
+from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 from matplotlib.ticker import FixedLocator
 import seaborn as sns
@@ -11,8 +12,12 @@ import itertools
 
 
 def cosine_similarity(vector_a: list, vector_b: list) -> float:
-    # print(vector_a)
-    # print(vector_b)
+    """
+    Finds the cosine similarity of two vectors
+    :param vector_a: The first vector to compare
+    :param vector_b: The second vector to compare
+    :return: The cosine similarity score of the two vectors
+    """
     return 1 - cosine(vector_a, vector_b)
 
 
@@ -22,11 +27,11 @@ def one_hot_encoding(df: pd.DataFrame) -> pd.DataFrame:
     :param df: Dataframe containing raw topics
     :return: Altered dataframe
     """
-    mapping: dict = {}
-    for index, topic in enumerate(df["Topic"].unique()):
-        mapping[topic] = index
+    topics: list = list(df["Topic"].unique())
+    mapping: dict = dict(zip(topics, range(len(topics))))
+    df["Topic Num"] = [mapping[topic] for topic in df['Topic'].values]
     print(mapping)
-    return df.replace({'Topic': mapping})
+    return df
 
 
 def generate_tsne_embeddings(fit_on, dimensions: int = 2) -> np.ndarray:
@@ -36,9 +41,15 @@ def generate_tsne_embeddings(fit_on, dimensions: int = 2) -> np.ndarray:
     :param dimensions: Num dimensions to reduce to
     :return: Generated embeddings
     """
-    return TSNE(n_components=dimensions, learning_rate='auto',
-                init='random', perplexity=30,
-                n_iter=1000, n_jobs=-1, random_state=42).fit_transform(fit_on)
+    return TSNE(
+        n_components=dimensions,
+        learning_rate="auto",
+        init="random",
+        perplexity=30,
+        n_iter=1000,
+        n_jobs=-1,
+        random_state=42,
+    ).fit_transform(fit_on)
 
 
 def display_tsne(x_col: str, y_col: str, df: pd.DataFrame) -> None:
@@ -49,19 +60,21 @@ def display_tsne(x_col: str, y_col: str, df: pd.DataFrame) -> None:
     :param df: Dataframe object holding the T-SNE embeddings of original text
     :return: None
     """
-    plt.figure(figsize=(6, 6))
-    plt.title("T-SNE dimensionality reduction of Contrasting Topics Dataset")
+    plt.figure(figsize=(8, 6))
+    plt.title("T-SNE dimensionality reduction")
     sns.scatterplot(
         x=x_col,
         y=y_col,
-        hue="Topic",
-        palette=sns.color_palette("hls", len(df["Topic"].unique())),
+        hue="Topic Num",
+        palette=sns.color_palette("hls", len(df["Topic Num"].unique())),
+        s=20,
+        ec="grey",
         data=df,
         legend="full",
-        alpha=0.75
+        alpha=0.5,
     )
-    plt.show()
-    plt.close()
+    plt.legend(labels=[f"{df['Topic Num'].unique()[i]} - {df['Topic'].unique()[i]}" for i in
+                       range(len(df['Topic Num'].unique()))])
 
 
 def visualize_matrix(data_to_visualize, axis_labels, title: str, cosine_sim: bool = False) -> None:
@@ -92,14 +105,14 @@ def visualize_matrix(data_to_visualize, axis_labels, title: str, cosine_sim: boo
     ax.yaxis.set_minor_locator(FixedLocator(ticks))
     ax.yaxis.set_major_locator(FixedLocator([(t0 + t1) / 2 for t0, t1 in zip(ticks[:-1], ticks[1:])]))
     ax.set_yticklabels(labels, rotation=0)
-    ax.tick_params(axis='both', which='major', length=0)
-    ax.tick_params(axis='y', which='minor', length=60)
+    ax.tick_params(axis="both", which="major", length=0)
+    ax.tick_params(axis="y", which="minor", length=60)
     plt.tight_layout()
     plt.show()
     plt.close()
 
 
-def average_similarity(df: pd.DataFrame, label_column: str = "Topic") -> dict:
+def average_similarity(df: pd.DataFrame, label_column: str = "Topic Num") -> dict:
     """
     Finds the average similarity of each topics phrases to each other
     :param df: Dataframe containing the embeddings and their labels
@@ -108,17 +121,22 @@ def average_similarity(df: pd.DataFrame, label_column: str = "Topic") -> dict:
     """
     averages: dict = {}
     for i in range(len(data[label_column].unique())):
-        topic_embeddings = np.array(list(df.loc[data[label_column] == i, 'Embedding'].values))
+        topic_embeddings = np.array(list(df.loc[data[label_column] == i, "Embedding"].values))
         avg: float = 0
         comparisons: int = 0
         for j, k in itertools.combinations(topic_embeddings, 2):
             avg += cosine_similarity(j, k)
             comparisons += 1
-        averages[f'{i}'] = avg / comparisons
+        averages[f"{i}"] = avg / comparisons
     return averages
 
 
 def cosine_matrix(vectors: np.ndarray) -> np.ndarray:
+    """
+    Given a list of vectors, find the cosine similarity between all of them and create a 2d matrix housing the values
+    :param vectors: The list of vectors to analyze
+    :return: Matrix of cosine similarities
+    """
     # print(len(vectors[0]))  # 768
     similarity_matrix: list = []
     for i in range(len(vectors)):
@@ -129,25 +147,78 @@ def cosine_matrix(vectors: np.ndarray) -> np.ndarray:
     return np.array(similarity_matrix)
 
 
-if __name__ == '__main__':
+def kmeans_cluster(x_col: str, y_col: str, df: pd.DataFrame, num_clusters: int) -> None:
+    """
+
+    :param x_col:
+    :param y_col:
+    :param df:
+    :param num_clusters:
+    :return:
+    """
+    kmeans = KMeans(n_clusters=num_clusters)
+    kmeans_data = list(zip(df[x_col].values, df[y_col].values))
+    kmeans.fit(kmeans_data)
+
+    plt.figure(figsize=(8, 6))
+    plt.title("K-Means Clustering")
+    ax = sns.scatterplot(
+        x=x_col,
+        y=y_col,
+        hue=kmeans.labels_,
+        palette=sns.color_palette("hls", len(df["Topic"].unique())),
+        data=df,
+        s=20,
+        ec="grey",
+        alpha=0.5,
+    )
+    ax = sns.scatterplot(
+        x=kmeans.cluster_centers_[:, 0],
+        y=kmeans.cluster_centers_[:, 1],
+        hue=range(num_clusters),
+        palette=sns.color_palette("hls", num_clusters),
+        s=45,
+        ec="black",
+        legend=False,
+        alpha=0.75,
+        ax=ax,
+    )
+    plt.legend(labels=[f"{df['Topic Num'].unique()[i]} - {df['Topic'].unique()[i]}" for i in
+                       range(len(df['Topic Num'].unique()))])
+
+
+if __name__ == "__main__":
     # preprocessing
-    data: pd.DataFrame = pd.read_csv('contrast-dataset.csv', index_col='ID')
-    topic_labels: list = list(data['Topic'].values)
+    data: pd.DataFrame = pd.read_csv("contrast-dataset.csv", index_col="ID")
+    topic_labels: list = list(data["Topic"].values)
     data = one_hot_encoding(data)
-    data['Embedding'] = data['Phrase'].apply(embed)
-    phrase_embeddings: np.ndarray = np.array(list(data['Embedding'].values))
+    data["Embedding"] = data["Phrase"].apply(embed)
+    phrase_embeddings: np.ndarray = np.array(list(data["Embedding"].values))
     # Distance matrix analysis
     euclid_dist_matrix: np.ndarray = distance_matrix(phrase_embeddings, phrase_embeddings, 2)  # create distance matrix
     # Cosine similarity analysis
     cosine_similarity_matrix: np.ndarray = cosine_matrix(phrase_embeddings)
     # tsne analysis
     tsne_embeddings: np.ndarray = generate_tsne_embeddings(phrase_embeddings)
-    data['tsne-2d-x'] = tsne_embeddings[:, 0]
-    data['tsne-2d-y'] = tsne_embeddings[:, 1]
-    # visualize
-    visualize_matrix(euclid_dist_matrix, topic_labels, "Distance matrix generated through sentence embeddings from contrasting topic dataset", cosine_sim=False)
-    visualize_matrix(cosine_similarity_matrix, topic_labels, "Cosine similarity across sentence embeddings from contrasting topic dataset", cosine_sim=True)
+    data["tsne-2d-x"] = tsne_embeddings[:, 0]
+    data["tsne-2d-y"] = tsne_embeddings[:, 1]
+    # # visualize
+    # visualize_matrix(
+    #     euclid_dist_matrix,
+    #     topic_labels,
+    #     "Distance matrix",
+    #     cosine_sim=False,
+    # )
+    # visualize_matrix(
+    #     cosine_similarity_matrix,
+    #     topic_labels,
+    #     "Cosine similarity",
+    #     cosine_sim=True,
+    # )
     display_tsne(x_col="tsne-2d-x", y_col="tsne-2d-y", df=data)
+    kmeans_cluster(x_col="tsne-2d-x", y_col="tsne-2d-y", df=data, num_clusters=len(data["Topic"].unique()))
+    plt.show()
+
     """
     Higher cosine score, more similar they are
     1 == perfect similarity ([1, 2, 3] and [1, 2, 3])
