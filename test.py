68ed11d6b7e7644487ad06eb0fee07071e2e00f6
VeiledTee <ethanheavey@outlook.com>
2023-06-30 23:08:06 -0300

    More testing

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import numpy as np
 from ripser import ripser
 from persim import plot_diagrams
+import matplotlib.pyplot as plt
 from typing import List
 import torch
 from transformers import BertTokenizer, BertModel
@@ -10,6 +11,12 @@ import pandas as pd
 from ripser import ripser
 from persim import plot_diagrams
 import plotly.graph_objs as gobj
+
+
+points = np.random.random((100, 2))
+f = d.fill_rips(points, 2, 1.)
+p = d.homology_persistence(f)
+dgms = d.init_diagrams(p, f)
 
 
 def plot_diagram(diagram, homology_dimensions=None, plotly_params=None):
@@ -146,7 +153,6 @@ def plot_diagram(diagram, homology_dimensions=None, plotly_params=None):
     return fig
 
 
-
 def get_bert_embeddings(sentence: str) -> List[float]:
     # Disable the logging level for the transformers library
     logging.getLogger("transformers").setLevel(logging.ERROR)
@@ -205,19 +211,53 @@ sentences = [
 #     "Josh hates cake"
 # ]
 
-data: pd.DataFrame = pd.read_csv("Data/contrast-dataset.csv")
+# data: pd.DataFrame = pd.read_csv("Data/contrast-dataset.csv")
+# sentences = data["Phrase"].values
 
-e = [get_bert_embeddings(s) for s in data["Phrase"].values]
-
-# Convert the list of BERT embeddings to a numpy array
-embeddings = np.array(e).T
-print(embeddings.shape)
-
-# Compute persistent homology using ripser
-diagrams = ripser(embeddings)['dgms']
-
-print(len(diagrams))
-print(diagrams[0].shape)
+# e = [get_bert_embeddings(s) for s in sentences]
+#
+# # Convert the list of BERT embeddings to a numpy array
+# embeddings = np.array(e).T
+# print(embeddings.shape)
+#
+# # Compute persistent homology using ripser
+# # Compute persistent homology using ripser
+# result = ripser(embeddings)
+# diagrams = result['dgms']
+#
+# # Create a figure with subplots
+# fig, axes = plt.subplots(len(diagrams), 1, figsize=(8, 6), sharex=True)
+#
+# # Iterate over all dimensions
+# for dim, features in enumerate(diagrams):
+#     # Extract birth and death values
+#     birth_values = [point[0] for point in features]
+#     death_values = [point[1] for point in features]
+#     sorted_death = sorted(death_values, reverse=True)
+#     print(sorted_death)
+#
+#     # Compute maximum epsilon value
+#     for i in sorted_death:
+#         if i != np.inf:
+#             max_epsilon = i
+#             break
+#
+#     # Plot the bar chart in the corresponding subplot
+#     ax = axes[dim]
+#     ax.bar(range(len(birth_values)), height=[point[1] - point[0] for point in features],
+#            align='center', alpha=0.5, color='blue', label=f'H{dim} Features')
+#     ax.set_ylabel('Persistence')
+#     ax.set_title(f'Persistent Homology H{dim} Bar Chart')
+#     ax.set_ylim(0, max_epsilon)  # Set y-axis limits
+#
+# # Set the x-axis label for the last subplot
+# axes[-1].set_xlabel('Feature Index')
+#
+# # Adjust spacing between subplots
+# plt.tight_layout()
+#
+# # Show the combined figure
+# plt.show()
 
 # # Extract birth and death values from the persistence diagrams
 # birth_values = [point[0] for point in diagrams[0]]
@@ -249,5 +289,29 @@ print(point_cloud)
 diagrams = ripser(point_cloud)['dgms']
 """
 # Plot the persistence diagrams
-plot_diagrams(diagrams, show=True)
+# plot_diagrams(diagrams, show=True)
 # """
+
+# import networkx as nx
+#
+# # Choose a filtration parameter (e.g., maximum distance)
+# filtration_parameter = 0.5
+#
+# # Construct the simplicial complex
+# G = nx.Graph()
+# for dim, diagram in enumerate(diagrams):
+#     for point in diagram:
+#         birth, death = point
+#         if death - birth > filtration_parameter:
+#             continue
+#         G.add_node(point)
+#         G.nodes[point]['birth'] = birth
+#         G.nodes[point]['death'] = death
+#
+# # Add edges based on connectivity
+# for edge in simplicial_complex:
+#     G.add_edge(edge[0], edge[1])
+#
+# # Visualize the graph
+# nx.draw(G, with_labels=True, node_size=200, font_size=8, edge_color='gray', node_color='lightblue')
+# plt.show()
