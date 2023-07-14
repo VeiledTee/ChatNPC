a63062584f13b88d085a1785ecba49415ebc9287
Veiled Tee <ethanheavey@outlook.com>
2023-07-13 22:59:04 -0300

    Embedding Pipeline

--- convert_dataset.py
+++ convert_dataset.py
@@ -1,6 +1,6 @@
 import concurrent.futures
 import logging
-import queue
+import os
 from typing import Dict, List, Tuple
 
 import numpy as np
@@ -14,7 +14,14 @@ logging.getLogger("transformers").setLevel(logging.ERROR)
 
 # Define hyperparameters
 SEQUENCE_LENGTH: int = 128
-BATCH_SIZE: int = 128
+BATCH_SIZE: int = 4
+# Check if GPU is available
+if torch.cuda.is_available():
+    DEVICE = torch.device("cuda")
+    print("GPU is available. PyTorch is using GPU:", torch.cuda.get_device_name(DEVICE))
+else:
+    print("GPU is not available. PyTorch is using CPU.")
+
 
 def load_txt_file_to_dataframe(dataset_description: str) -> pd.DataFrame:
     """
@@ -51,142 +58,101 @@ def load_txt_file_to_dataframe(dataset_description: str) -> pd.DataFrame:
     return data_frame
 
 
-def split_into_batches(df: pd.DataFrame) -> List[List[List[str | int]]]:
-    num_batches = len(df) // BATCH_SIZE
-    batches = []
+def parallel_get_bert_embeddings(batches):
+    embeddings = []
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        total_batches = len(batches)
+        with tqdm(total=total_batches, desc="Processing batches") as pbar:
+            for i, batch in enumerate(batches):
+                futures = []
+                ids = batch[3]
+                for b in range(len(batch[0])):
+                    future = executor.submit(get_bert_embeddings, batch[0][b], batch[1][b])
+                    futures.append(future)
+                print(len([future.result() for future in futures]))
+                results = np.array([future.result() for future in futures]).squeeze()
+                embeddings.extend(results)
+                pbar.update(1)
 
-    gold_label: List[int] = [1 if x == "contradiction" else 0 for x in df["gold_label"]]
+                # Save embeddings from each batch with corresponding IDs as keys
+                batch_output_file = f"Data/NPZ/batch_{i}.npz"
+                np.savez_compressed(batch_output_file, **{str(identity): emb for identity, emb in zip(ids, results)})
 
-    for i in range(num_batches):
-        start_index = i * BATCH_SIZE
-        end_index = start_index + BATCH_SIZE
-
-        batch: Tuple[List[List[str]], List[List[str]], List[int], List[int]] = (
-            df["sentence1"].iloc[start_index:end_index].tolist(),
-            df["sentence2"].iloc[start_index:end_index].tolist(),
-            gold_label[start_index:end_index],
-            df.index[start_index:end_index].tolist(),
-        )
-
-        batches.append(batch)
-
-    # Handle the remaining rows if the dataframe size is not divisible by BATCH_SIZE
-    if len(df) % BATCH_SIZE != 0:
-        start_index = num_batches * BATCH_SIZE
-        batch: Tuple[List[List[str]], List[List[str]], List[int], List[int]] = (
-            df["sentence1"].iloc[start_index:].tolist(),
-            df["sentence2"].iloc[start_index:].tolist(),
-            gold_label[start_index:],
-            df.index[start_index:].tolist(),
-        )
-
-        batches.append(batch)
-
-    return batches
-
-
-def save_embeddings(arr: np.ndarray, IDs: np.ndarray, filename: str) -> None:
-    # Check if the file already exists
-    try:
-        existing_data = np.load(filename)
-
-        # If the file exists, extract the existing data
-        existing_arr = existing_data["arr"]
-        existing_ids = existing_data["IDs"]
-
-        # Reshape the existing_arr to have the same dimensions as arr
-        existing_arr = np.reshape(existing_arr, (existing_arr.shape[0],) + arr.shape[1:])
-
-        # Append the new data to the existing arrays
-        arr = np.concatenate((existing_arr, arr))
-        existing_ids += IDs[:existing_ids.shape[0]]
-        IDs = existing_ids
-    except FileNotFoundError:
-        pass
-
-    # Split the array into individual arrays
-    split_arrays = np.vsplit(arr, arr.shape[0])
-
-    # Save each split array with its corresponding ID
-    np.savez_compressed(filename, arr=split_arrays, IDs=IDs)
+    return np.array(embeddings)
 
 
 def get_bert_embeddings(sentence1: str, sentence2: str) -> np.ndarray:
     # Load pre-trained BERT model and tokenizer
-    tokenizer: BertTokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
-    model: BertModel = BertModel.from_pretrained("bert-base-uncased")
+    tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+    model = BertModel.from_pretrained("bert-base-uncased")
+
+    # Move model to the specified device
+    model.to(DEVICE)
 
     # Tokenize the sentences and obtain the input IDs and attention masks
-    tokens: Dict[str, torch.Tensor] = tokenizer.encode_plus(
-        sentence1, sentence2, add_special_tokens=True, padding="longest", truncation=True
+    encoding = tokenizer.encode_plus(
+        sentence1, sentence2, add_special_tokens=True, padding="longest", truncation=True, return_tensors="pt"
     )
-    input_ids: torch.Tensor = torch.tensor(tokens["input_ids"]).unsqueeze(0)  # Add batch dimension
-    attention_mask: torch.Tensor = torch.tensor(tokens["attention_mask"]).unsqueeze(0)  # Add batch dimension
+    input_ids = encoding["input_ids"].to(DEVICE)
+    attention_mask = encoding["attention_mask"].to(DEVICE)
 
     # Pad or truncate the input IDs and attention masks to the maximum sequence length
     input_ids = torch.nn.functional.pad(input_ids, (0, SEQUENCE_LENGTH - input_ids.size(1)))
     attention_mask = torch.nn.functional.pad(attention_mask, (0, SEQUENCE_LENGTH - attention_mask.size(1)))
 
-    # Obtain the BERT embeddings
+    # Obtain the BERT embeddings and convert to NumPy array on CPU
     with torch.no_grad():
-        bert_outputs: Tuple[torch.Tensor] = model(input_ids, attention_mask=attention_mask)
-        embeddings: torch.Tensor = bert_outputs.last_hidden_state  # Extract the last hidden state
+        outputs = model(input_ids, attention_mask=attention_mask)
+        embeddings = outputs.last_hidden_state.cpu().numpy()
 
-    return embeddings.numpy()
-
-
-def process_batch(batch):
-    sentenceA, sentenceB = batch[0], batch[1]
-    x_train = []
-
-    q = queue.Queue(maxsize=16)  # Set the maximum number of concurrent threads to 16
-
-    with concurrent.futures.ThreadPoolExecutor(max_workers=8) as exe:
-        futures = []
-
-        for i in range(len(sentenceA)):
-            # Submit the task to the executor and add the future object to the list
-            futures.append(exe.submit(get_bert_embeddings, sentenceA[i], sentenceB[i]))
-
-            # Check if the maximum number of concurrent threads is reached
-            if len(futures) >= 16:
-                # Wait for the first completed future and retrieve the result
-                completed = concurrent.futures.as_completed(futures)
-                for future in completed:
-                    x_train.append(future.result())  # Append the result to the list
-                    futures.remove(future)  # Remove the completed future
-
-        # Process the remaining futures
-        for future in concurrent.futures.as_completed(futures):
-            x_train.append(future.result())  # Append the result to the list
-
-    arrays = np.array([np.squeeze(x) for x in x_train])
-    return arrays
+    return embeddings
 
 
-if __name__ == "__main__":
-    # Check if GPU is available
-    if torch.cuda.is_available():
-        device = torch.device("cuda")
-        print("GPU is available. PyTorch is using GPU:", torch.cuda.get_device_name(device))
-    else:
-        print("GPU is not available. PyTorch is using CPU.")
+def group_rows(dataframe):
+    grouped_data = []
+    for i in range(0, len(dataframe), BATCH_SIZE):
+        batch = dataframe.iloc[i:i+BATCH_SIZE]
+        sentenceA: List[str] = [x for x in batch["sentence1"]]
+        sentenceB: List[str] = [x for x in batch["sentence2"]]
+        labels: List[int] = [1 if x == "contradiction" else 0 for x in batch["gold_label"]]
+        ids: List[int] = list(batch.index)
+        grouped_data.append([sentenceA, sentenceB, labels, ids])
+    return grouped_data
 
-    # load data
-    multinli_df: pd.DataFrame = load_txt_file_to_dataframe("train")  # all train
-    # multinli_df: pd.DataFrame = load_txt_file_to_dataframe('match')  # 10 rows of match
-    # multinli_df: pd.DataFrame = load_txt_file_to_dataframe('mismatch')  # all mismatch
-    print(len(multinli_df.index))
-    data_batches = split_into_batches(multinli_df)
-    print(len(data_batches))
-    embeddings = []
 
-    with tqdm(total=len(data_batches)) as pbar:
-        with concurrent.futures.ThreadPoolExecutor(max_workers=8) as executor:
-            results = executor.map(process_batch, data_batches)
-            for result, batch in zip(results, data_batches):
-                embeddings.append(result)
-                pbar.update(1)
-                save_embeddings(result, np.array(batch[3]), "Data/MultiNLI/training.npz")
+def combine_npz_files(directory: str = "Data/NPZ", output_file: str = "Data/MultiNLI/batch_sum.npz"):
+    npz_files = [file for file in os.listdir(directory) if file.endswith('.npz')]
+    combined_data = {}
 
-    # embeddings = np.concatenate(embeddings)
+    for file in npz_files:
+        retrieved = read_npz_file(os.path.join(directory, file))
+        for key, array in retrieved.items():
+            combined_data[key] = array
+    np.savez_compressed(output_file, **combined_data)
+
+
+def read_npz_file(file_path):
+    loaded_data = np.load(file_path)
+    data = {}
+
+    for key in loaded_data.files:
+        data[key] = loaded_data[key]
+
+    return data
+
+
+if __name__ == '__main__':
+    for dir in ["Data/NPZ", "Data/MultiNLI"]:
+        if not os.path.exists(dir):
+            os.makedirs(dir)
+    multinli_df: pd.DataFrame = load_txt_file_to_dataframe('match')  # 10 rows of match
+    print(f"\nRow count: {len(multinli_df)}")
+    data_batches = group_rows(multinli_df)
+    print(f"Num Batches: {len(data_batches)}")
+    embedded_batches = parallel_get_bert_embeddings(data_batches)
+    print(f"Embedding count: {len(embedded_batches)}")
+    print(f"Embedding shape: {embedded_batches.shape}")
+    combine_npz_files()
+    retrieved = read_npz_file("Data/MultiNLI/batch_sum.npz")
+    for k, array in retrieved.items():
+        print(f"Array with key '{k}': {np.array_equal(embedded_batches[int(k)], array)}")
