53184dbfc00a413bab1e0a6f410d4c1f14e471b7
Veiled Tee <ethanheavey@outlook.com>
2023-07-15 15:51:51 -0300

    Variables file to keep track of globals

---
+++ variables.py
@@ -0,0 +1,24 @@
+import math
+import torch
+
+#  Specify dataset to use (train/match/mismatch)
+DATASET = 'mismatch'
+
+# Define hyperparameters
+INPUT_SIZE: int = 768
+SEQUENCE_LENGTH: int = 128
+HIDDEN_SIZE: int = 64
+NUM_LAYERS: int = 2
+OUTPUT_SIZE: int = 1
+EPOCHS: int = 250
+LEARNING_RATE: float = 0.001
+CHKPT_INTERVAL: int = int(math.ceil(EPOCHS / 10))
+if DATASET == 'train':
+    BATCH_SIZE: int = 1024
+else:
+    BATCH_SIZE: int = 128
+# Set pytorch device
+if torch.cuda.is_available():
+    DEVICE = torch.device("cuda")
+else:
+    DEVICE = torch.device('cpu')
