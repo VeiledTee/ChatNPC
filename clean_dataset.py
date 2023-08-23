fa5f561ddfdc5103ff7891cad49219fa5699c52b
Veiled Tee <ethanheavey@outlook.com>
2023-08-23 12:44:52 -0300

    Rework PH stuff

--- clean_dataset.py
+++ clean_dataset.py
@@ -3,50 +3,77 @@ import numpy as np
 
 from ContradictDetectNN import get_sentence_embedding, count_negations
 from persitent_homology import persistent_homology_features
+from tqdm import tqdm
+import csv
 
 
-def embed_and_ph(df: pd.DataFrame) -> pd.DataFrame:
-    df["label"] = df["gold_label"].str.lower()
-    df["label"] = np.where(df["label"] == "contradiction", 1, 0)
-    print("labels complete")
-    # Apply the get_sentence_embedding function to generate embeddings
-    if "sentence1_embeddings" in df.columns:
-        print("sentence1_embeddings already exists")
-    else:
-        df["sentence1_embeddings"] = df["sentence1"].apply(lambda x: get_sentence_embedding(x))
-        print("sentence1_embeddings complete")
-    s1_ph_features = persistent_homology_features(list(df["sentence1"]))
-    df["sentence1_ph_a"] = [item[0] for item in s1_ph_features]
-    df["sentence1_ph_b"] = [item[1] for item in s1_ph_features]
-    print("sentence1_features complete")
-    if "sentence2_embeddings" in df.columns:
-        print("sentence2_embeddings already exists")
-    else:
-        df["sentence2_embeddings"] = df["sentence2"].apply(lambda x: get_sentence_embedding(x))
-        print("sentence2_embeddings complete")
-    s2_ph_features = persistent_homology_features(list(df["sentence2"]))
-    df["sentence2_ph_a"] = [item[0] for item in s2_ph_features]
-    df["sentence2_ph_b"] = [item[1] for item in s2_ph_features]
-    print("sentence2_features complete")
-    # Apply the count_negations function to generate negation counts
-    df["negation"] = df[["sentence1", "sentence2"]].apply(lambda x: count_negations(x.tolist()), axis=1)
-    print("negation complete")
-    # for index, row in df.iterrows():
-    #     print(f"Index: {index}")
-    #     print(f"Sentence 1: {row['sentence1']}")
-    #     print(f"Sentence 2: {row['sentence2']}")
-    #     print(f"Label: {row['label']}")
-    #     print(f"Negation Count: {row['negation']}")
-    #     print(f"Sentence 1 Embedding: {row['sentence1_embeddings'].shape}")
-    #     print(f"Sentence 2 Embedding: {row['sentence2_embeddings'].shape}")
-    #     print("\n")
-    return df
+# def embed_and_ph(df: pd.DataFrame) -> pd.DataFrame:
+#     df["label"] = df["gold_label"].str.lower()
+#     df["label"] = np.where(df["label"] == "contradiction", 1, 0)
+#     print("labels complete")
+#
+#     s1_ph_features = persistent_homology_features(list(df["sentence1"]))
+#     df["sentence1_ph_a"] = [item[0] for item in s1_ph_features]
+#     df["sentence1_ph_b"] = [item[1] for item in s1_ph_features]
+#     print("sentence1_features complete")
+#
+#     s2_ph_features = persistent_homology_features(list(df["sentence2"]))
+#     df["sentence2_ph_a"] = [item[0] for item in s2_ph_features]
+#     df["sentence2_ph_b"] = [item[1] for item in s2_ph_features]
+#     print("sentence2_features complete")
+#     # Apply the count_negations function to generate negation counts
+#     df["negation"] = df[["sentence1", "sentence2"]].apply(lambda x: count_negations(x.tolist()), axis=1)
+#     print("negation complete")
+#     return df
+
+
+def embed_and_ph(df_for_cleaning: pd.DataFrame, output_csv_path: str) -> None:
+    for i, row in tqdm(df_for_cleaning.iterrows(), total=len(df_for_cleaning)):
+        label = 1 if row['gold_label'].lower() == 'contradiction' else 0
+
+        # Apply the get_sentence_embedding function to generate embeddings
+        if 'sentence1_embeddings' not in row:
+            row['sentence1_embeddings'] = get_sentence_embedding(row['sentence1'])
+
+        if 'sentence2_embeddings' not in row:
+            row['sentence2_embeddings'] = get_sentence_embedding(row['sentence2'])
+
+        if 'sentence1_ph_a' not in row:
+            s1_ph_features = persistent_homology_features([row['sentence1']])
+            row['sentence1_ph_a'] = s1_ph_features[0][0]
+            row['sentence1_ph_b'] = s1_ph_features[0][1]
+
+        if 'sentence2_ph_a' not in row:
+            s2_ph_features = persistent_homology_features([row['sentence2']])
+            row['sentence2_ph_a'] = s2_ph_features[0][0]
+            row['sentence2_ph_b'] = s2_ph_features[0][1]
+
+        # Calculate negation count
+        negation_count = count_negations([row['sentence1'], row['sentence2']])
+
+        # Create a new row for the result DataFrame
+        result_row = {
+            "label": label,
+            "sentence1_embeddings": row['sentence1_embeddings'],
+            "sentence2_embeddings": row['sentence2_embeddings'],
+            "sentence1_ph_a": row['sentence1_ph_a'],
+            "sentence1_ph_b": row['sentence1_ph_b'],
+            "sentence2_ph_a": row['sentence2_ph_a'],
+            "sentence2_ph_b": row['sentence2_ph_b'],
+            "negation": negation_count
+        }
+
+        with open(output_csv_path, 'a', newline='') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=result_row.keys())
+            if i == 0:
+                writer.writeheader()  # Write the header only for the first row
+            writer.writerow(result_row)
 
 
 if __name__ == "__main__":
-    TO_CLEAN: list = ["Data/match_cleaned.csv", "Data/mismatch_cleaned.csv"]
+    TO_CLEAN: list = ["Data/mismatch_cleaned.csv", "Data/match.csv"]
     for file in TO_CLEAN:
         print(f"\tIn Progress: {file}")
         df = pd.read_csv(file)
-        df = embed_and_ph(df)
-        df.to_csv(f"{file[:-4]}_ph.csv", index=False)
+        embed_and_ph(df, f"{file[:-4]}_ph.csv")
+        # df.to_csv(f"{file[:-4]}_ph.csv", index=False)
